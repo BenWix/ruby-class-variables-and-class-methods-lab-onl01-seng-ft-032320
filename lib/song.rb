5ce7a98e9d25143652061240ec89b2dbac50a0fd
Benjamin Wichser <benwichser@gmail.com>
2020-03-27 19:19:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name ,  :artist , :genre 
   
   @@count = 0 
+  @@genres = []
   
-  GENRES = []
   def initialize(name,artist,genre)
     @count += 1 
     @name = name 
